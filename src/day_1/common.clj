b4b9aaa76f3ca34d0accac0bded0cb002d3b1dcf
Janne Sauvala <janne.sauvala@gmail.com>
2019-12-01 19:52:07 +0200

    Extract function

--- src/day_1/common.clj
+++ src/day_1/common.clj
@@ -1,11 +1,16 @@
 (ns day-1.common
   (:require [clojure.string :as str]))
 
+(defn fuel-amount
+  [masses]
+  (->> masses
+    (map #(int (Math/floor (/ % 3))))
+    (map #(- % 2))))
+
 (defn calculate-fuel-for-modules
   [input]
   (->> input
        (str/split-lines)
        (map #(Integer/parseInt %))
-       (map #(int (Math/floor (/ % 3))))
-       (map #(- % 2))
+       (fuel-amount)
        (reduce +)))
